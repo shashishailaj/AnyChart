1dfd79e8543b321464171af5f0be42557446ec72
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2019-02-01 11:12:01 +0800

    QA fix for text marker zindex and live edit on range markers

--- src/gantt/Timeline.js
+++ src/gantt/Timeline.js
@@ -1769,6 +1769,7 @@ anychart.ganttModule.TimeLine.prototype.lineMarker = function(opt_indexOrValue, 
   if (!lineMarker) {
     lineMarker = new anychart.ganttModule.axisMarkers.Line(this.scale_);
     lineMarker.setup(this.defaultLineMarkerSettings());
+    lineMarker.setParentEventTarget(this);
     this.lineMarkers_[index] = lineMarker;
     lineMarker.listenSignals(this.onMarkersSignal_, this);
     this.invalidate(anychart.ConsistencyState.TIMELINE_MARKERS, anychart.Signal.NEEDS_REDRAW);
@@ -1804,6 +1805,7 @@ anychart.ganttModule.TimeLine.prototype.rangeMarker = function(opt_indexOrValue,
   if (!rangeMarker) {
     rangeMarker = new anychart.ganttModule.axisMarkers.Range(this.scale_);
     rangeMarker.setup(this.defaultRangeMarkerSettings());
+    rangeMarker.setParentEventTarget(this);
     this.rangeMarkers_[index] = rangeMarker;
     rangeMarker.listenSignals(this.onMarkersSignal_, this);
     this.invalidate(anychart.ConsistencyState.TIMELINE_MARKERS, anychart.Signal.NEEDS_REDRAW);
@@ -1838,6 +1840,7 @@ anychart.ganttModule.TimeLine.prototype.textMarker = function(opt_indexOrValue, 
   if (!textMarker) {
     textMarker = new anychart.ganttModule.axisMarkers.Text(this.scale_);
     textMarker.setup(this.defaultTextMarkerSettings());
+    textMarker.setParentEventTarget(this);
     this.textMarkers_[index] = textMarker;
     textMarker.listenSignals(this.onMarkersSignal_, this);
     this.invalidate(anychart.ConsistencyState.TIMELINE_MARKERS, anychart.Signal.NEEDS_REDRAW);
@@ -4739,7 +4742,6 @@ anychart.ganttModule.TimeLine.prototype.initScale = function() {
  */
 anychart.ganttModule.TimeLine.prototype.initDom = function() {
   this.getClipLayer().zIndex(anychart.ganttModule.BaseGrid.DRAW_Z_INDEX - 2); //Put it under draw layer.
-  this.getMarkersLayer().zIndex(anychart.ganttModule.BaseGrid.DRAW_Z_INDEX - 1); //Put it under draw layer but above clip layer.
   this.markers().container(this.getContentLayer());
   this.labels().container(this.getContentLayer());
   this.header().container(this.getBase());
@@ -4762,6 +4764,48 @@ anychart.ganttModule.TimeLine.prototype.boundsInvalidated = function() {
  */
 anychart.ganttModule.TimeLine.prototype.appearanceInvalidated = function() {
   this.getSeparationPath_().stroke(/** @type {acgraph.vector.Stroke} */(anychart.ganttModule.BaseGrid.getColorResolver('columnStroke', anychart.enums.ColorType.STROKE, false)(this, 0)));
+};
+
+
+/**
+ * Inner getter for this.rangeLineMarkersLayer_.
+ * @return {acgraph.vector.Layer}
+ */
+anychart.ganttModule.TimeLine.prototype.getRangeLineMarkersLayer = function() {
+  if (!this.rangeLineMarkersLayer_) {
+    this.rangeLineMarkersLayer_ = /** @type {acgraph.vector.Layer} */ (acgraph.layer());
+    this.rangeLineMarkersLayer_.zIndex(anychart.ganttModule.BaseGrid.DRAW_Z_INDEX - 1);
+  }
+  return this.rangeLineMarkersLayer_;
+};
+
+
+/**
+ * Inner getter for this.drawLayer_.
+ * @return {acgraph.vector.Layer}
+ */
+anychart.ganttModule.TimeLine.prototype.getTextMarkersLayer = function() {
+  if (!this.textMarkersLayer_) {
+    this.textMarkersLayer_ = /** @type {acgraph.vector.Layer} */ (acgraph.layer());
+    this.textMarkersLayer_.zIndex(anychart.ganttModule.BaseGrid.DRAW_Z_INDEX + 1);
+  }
+  return this.textMarkersLayer_;
+};
+
+
+/**
+ * @inheritDoc
+ */
+anychart.ganttModule.TimeLine.prototype.initLayersStructure = function(base) {
+  base
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getCellsLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getRangeLineMarkersLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getDrawLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getTextMarkersLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getContentLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getEditLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getClipLayer()))
+      .addChild(/** @type {!acgraph.vector.Layer} */ (this.getScrollsLayer()));
 };
 
 
@@ -4840,22 +4884,7 @@ anychart.ganttModule.TimeLine.prototype.positionFinal = function() {
   if (this.redrawPosition || this.redrawHeader) {
     this.drawTimelineElements_();
     this.invalidate(anychart.ConsistencyState.TIMELINE_ELEMENTS_LABELS);
-
     this.redrawHeader = false;
-
-    // var dataBounds = new anychart.math.Rect(this.pixelBoundsCache.left,
-    //     (this.pixelBoundsCache.top + /** @type {number} */ (this.headerHeight()) + 1),
-    //     this.pixelBoundsCache.width,
-    //     this.totalGridsHeight);
-    //
-    // for (var i = 0; i < this.textMarkers_.length; i++) {
-    //   var textMarker = this.textMarkers_[i];
-    //   textMarker.suspendSignalsDispatching();
-    //   textMarker.container(this.getMarkersLayer());
-    //   textMarker.parentBounds(dataBounds);
-    //   textMarker.resumeSignalsDispatching(false);
-    //   textMarker.draw();
-    // }
   }
 };
 
@@ -4873,21 +4902,19 @@ anychart.ganttModule.TimeLine.prototype.markersInvalidated = function() {
         this.pixelBoundsCache.width,
         (this.pixelBoundsCache.height - /** @type {number} */ (this.headerHeight()) - 1));
 
-    var textDataBounds = new anychart.math.Rect(this.pixelBoundsCache.left,
-        top,
-        this.pixelBoundsCache.width,
-        this.totalGridsHeight);
-
     for (var m = 0, count = markers.length; m < count; m++) {
       var axesMarker = markers[m];
       if (axesMarker) {
-        var b = acgraph.utils.instanceOf(axesMarker, anychart.ganttModule.axisMarkers.Text) ?
-            textDataBounds :
-            dataBounds;
+        var b = dataBounds;
+        var cont = this.getRangeLineMarkersLayer();
+        if (acgraph.utils.instanceOf(axesMarker, anychart.ganttModule.axisMarkers.Text)) {
+          b = new anychart.math.Rect(this.pixelBoundsCache.left, top, this.pixelBoundsCache.width, this.totalGridsHeight);
+          cont = this.getTextMarkersLayer();
+        }
         axesMarker.suspendSignalsDispatching();
         axesMarker.invalidate(anychart.ConsistencyState.BOUNDS);
         axesMarker.parentBounds(b);
-        axesMarker.container(this.getMarkersLayer());
+        axesMarker.container(cont);
         axesMarker.invalidate(anychart.ConsistencyState.CONTAINER); //Force set of parent.
         axesMarker.draw();
         axesMarker.resumeSignalsDispatching(false);
