512fb5939a979bfabe06e5105ed175aefbb66f96
Ilya Kurnoy <ilyakurnnoj@gmail.com>
2019-02-19 16:18:41 +0800

    theme changes for timeline series

--- src/timeline/defaultTheme.js
+++ src/timeline/defaultTheme.js
@@ -3,9 +3,14 @@ goog.provide('anychart.timelineModule.defaultTheme');
 goog.mixin(goog.global['anychart']['themes']['defaultTheme'], {
   'timeline': {
     'defaultSeriesSettings': {
+      'event': {
+        'length': '10%',
+        'stroke': anychart.core.defaultTheme.colorStrokeNormal
+      },
       'range': {
         'height': '10%'
       }
+
     }
   }
 });
